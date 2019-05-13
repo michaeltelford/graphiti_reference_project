de02bf04ee11546d6d8c5f3c2e69a72a9c1f1a6e
Michael Telford <michael.telford@live.com>
2019-05-13 20:16:59 +1000

    Added topic_id attribute to post resource

--- app/resources/post_resource.rb
+++ app/resources/post_resource.rb
@@ -2,6 +2,7 @@ class PostResource < ApplicationResource
   attribute :title, :string
   attribute :upvotes, :integer
   attribute :active, :boolean
+  attribute :topic_id, :integer
 
   belongs_to :topic
   has_many :comments
