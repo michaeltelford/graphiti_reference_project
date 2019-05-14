d57dd732642e93c520e68c5fe4ed0220c7bdcf49
Michael Telford <michael.telford@live.com>
2019-05-14 17:47:33 +1000

    Added custom attributes and stats to PostResource

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :topic
   has_many :comments
+
+  def title_upcase
+    title.upcase
+  end
+
+  def time_since_creation
+    Time.now - created_at
+  end
 end
--- app/resources/post_resource.rb
+++ app/resources/post_resource.rb
@@ -1,10 +1,17 @@
 class PostResource < ApplicationResource
   attribute :title, :string
+  attribute :title_upcase, :string
   attribute :upvotes, :integer
   attribute :downvotes, :integer
   attribute :active, :boolean
   attribute :topic_id, :integer
 
+  extra_attribute :age, :float do
+    @object.time_since_creation
+  end
+
   belongs_to :topic
   has_many :comments
+
+  stat upvotes: [:minimum, :maximum]
 end
